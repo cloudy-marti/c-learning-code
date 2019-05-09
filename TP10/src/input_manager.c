ae83f977c0536bb0a75d1a18e1d7112881cedc30
cloudy <cloud.kira@gmail.com>
2019-05-09 10:53:13 +0200

    cheeki breeky slider opa

--- TP10/src/input_manager.c
+++ TP10/src/input_manager.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <MLV/MLV_all.h>
 
 #include "../headers/slider.h"
+#include "../headers/mlv_board.h"
+#include "../headers/input_manager.h"
 
 int stdin_scan_input()
 {
@@ -16,4 +19,30 @@ int stdin_scan_input()
 	}
 
 	return input;
+}
+
+int click_input()
+{
+	int x = 0;
+	int y = 0;
+
+	int input = -1;
+
+	MLV_wait_mouse(&x, &y);
+
+	input = get_position(x, y);
+
+	return input;
+}
+
+int get_position(int x, int y)
+{
+	int position = 0;
+
+	x /= (CONSOLE_SIZE / PUZZLE_SIZE);
+	y /= (CONSOLE_SIZE / PUZZLE_SIZE);
+
+	position = (y*PUZZLE_SIZE) + x;
+
+	return position;
 }
